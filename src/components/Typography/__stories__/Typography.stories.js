7fb7951dfdb78875b4f25786a05821c907ce00da
Nick Johnson <8239566+nickjohnson-dev@users.noreply.github.com>
2020-05-29 11:56:04 -0500

    feat: Add Typography component

---
+++ src/components/Typography/__stories__/Typography.stories.js
@@ -0,0 +1,7 @@
+import Typography from '../index'
+export { default as Basics } from './TypographyBasics.story'
+
+export default {
+  title: 'Typography',
+  component: Typography,
+}
---
+++ src/components/Typography/index.vue
@@ -0,0 +1,150 @@
+<template>
+  <component :class="classes" :is="is"><slot /></component>
+</template>
+
+<script>
+import includes from 'lodash/includes'
+
+export default {
+  props: {
+    component: { type: String },
+    variant: {
+      default: 'body-large',
+      validator: getIsVariantValid,
+    },
+  },
+  computed: {
+    classes() {
+      return ['root', this.variant]
+    },
+    is() {
+      return (
+        this.component ||
+        {
+          'body-extra-small': 'span',
+          'body-large': 'span',
+          'body-small': 'span',
+          button: 'span',
+          h1: 'h1',
+          h2: 'h2',
+          h3: 'h3',
+          h4: 'h4',
+          h5: 'h5',
+          h6: 'h6',
+          'label-large': 'label',
+          'label-small': 'label',
+        }[this.variant]
+      )
+    },
+  },
+}
+
+function getIsVariantValid(value) {
+  const isValid = includes(
+    [
+      'body-extra-small',
+      'body-large',
+      'body-small',
+      'button',
+      'h1',
+      'h2',
+      'h3',
+      'h4',
+      'h5',
+      'h6',
+      'label-large',
+      'label-small',
+    ],
+    value,
+  )
+
+  if (!isValid) {
+    // eslint-disable-next-line no-console
+    console.error(
+      'Typography: The "variant" prop must be one of the following:',
+      String([
+        'body-extra-small',
+        'body-large',
+        'body-small',
+        'button',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'label-large',
+        'label-small',
+      ]),
+    )
+  }
+
+  return isValid
+}
+</script>
+
+<style scoped>
+.root {
+  display: block;
+}
+.body-extra-small {
+  @apply text-body-extra-small;
+  font-weight: 400;
+  line-height: 0.875rem;
+}
+.body-large {
+  @apply text-body-large;
+  font-weight: 400;
+  line-height: 1.188rem;
+}
+.body-small {
+  @apply text-body-small;
+  font-weight: 400;
+  line-height: 1.06rem;
+}
+.button {
+  @apply text-button;
+  font-weight: 700;
+  line-height: 1.188rem;
+}
+.h1 {
+  @apply text-h1;
+  font-weight: 700;
+  line-height: 8.25rem;
+}
+.h2 {
+  @apply text-h2;
+  font-weight: 600;
+  line-height: 4.19rem;
+}
+.h3 {
+  @apply text-h3;
+  font-weight: 600;
+  line-height: 3.625rem;
+}
+.h4 {
+  @apply text-h4;
+  font-weight: 600;
+  line-height: 2.375rem;
+}
+.h5 {
+  @apply text-h5;
+  font-weight: 600;
+  line-height: 1.625rem;
+}
+.h6 {
+  @apply text-h6;
+  font-weight: 400;
+  line-height: 1.5rem;
+}
+.label-large {
+  @apply text-label-large;
+  font-weight: 600;
+  line-height: 1rem;
+}
+.label-small {
+  @apply text-label-small;
+  font-weight: 600;
+  line-height: 0.875rem;
+}
+</style>

aa4d8607104a5604b6a3d53a7fae6814ab2a237f
James Parkes <parkesrjames@gmail.com>
2020-05-29 09:48:05 -0500

    feat: Buttons update

--- src/components/Button/__stories__/Button.stories.js
+++ src/components/Button/__stories__/Button.stories.js
@@ -1,8 +1,47 @@
+import { select, text } from '@storybook/addon-knobs'
+
 import Button from '../index'
-export { default as Ghost } from './ButtonGhost.story'
-export { default as Text } from './ButtonText.story'
 
 export default {
   title: 'Button',
   component: Button,
 }
+
+export const Default = () => ({
+  components: { Button },
+  template: `
+    <Button
+      @click.native="action"
+      :class="select">
+      {{ text }}
+    </Button>
+  `,
+  props: {
+    text: {
+      default: text('Text', 'Button Text'),
+    },
+    select: {
+      default: select(
+        'Type',
+        {
+          Primary: 'primary',
+          Secondary: 'secondary',
+          Danger: 'danger',
+        },
+        'primary',
+      ),
+    },
+  },
+  methods: { action: () => console.log('Clicked!') },
+})
+
+export const Icon = () => ({
+  components: { Button },
+  template: `
+    <Button
+      @click.native="action">
+      +
+    </Button>
+  `,
+  methods: { action: () => console.log('Test!') },
+})
--- src/components/Button/index.vue
+++ src/components/Button/index.vue
@@ -6,8 +6,35 @@
 export default {}
 </script>
 
-<style>
+<style scoped>
 .root {
-  @apply h-10 bg-light-watermelon text-blue-jeans;
+  @apply w-32 p-2 rounded-lg;
+}
+
+.root:active {
+  @apply bg-heavy-black;
+}
+
+.root:focus {
+  @apply bg-heavy-black outline-none;
+}
+
+.root:disabled {
+  @apply bg-grey-300;
+}
+
+.root:hover {
+  @apply underline;
+}
+.primary {
+  @apply text-white bg-black;
+}
+
+.secondary {
+  @apply text-black bg-white;
+}
+
+.danger {
+  @apply text-white bg-state-error-100;
 }
 </style>

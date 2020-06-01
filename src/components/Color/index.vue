c98f3c9acf70dd7026c7168159ddc7b5dab789b7
jamesparkes <parkesrjames@gmail.com>
2020-06-01 10:21:22 -0500

    add surface colors

--- src/components/Color/index.vue
+++ src/components/Color/index.vue
@@ -1,10 +1,49 @@
 <template>
-  <div class="w-2 h-2">Color</div>
+  <Typography v-if="name" variant="body-extra-small">
+    <div :class="classes">
+      <div>{{ name }}</div>
+      <div>#{{ hex }}</div>
+    </div>
+  </Typography>
 </template>
 
 <script>
-export default {}
+import Typography from '../Typography/index'
+
+export default {
+  props: {
+    hex: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+  },
+  components: {
+    Typography,
+  },
+  computed: {
+    classes() {
+      return ['swatch', `swatch--color-${this.name}`]
+    },
+  },
+}
 </script>
 
 <style scoped>
+.swatch {
+  @apply flex items-center capitalize justify-between p-4 h-12;
+}
+
+.swatch--color-white {
+  @apply bg-white text-black;
+}
+
+.swatch--color-black {
+  @apply bg-black text-white;
+}
+
+.swatch--color-grey-200 {
+  @apply bg-grey-200 text-black;
+}
 </style>

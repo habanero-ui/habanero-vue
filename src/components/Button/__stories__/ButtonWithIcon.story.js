8698302616c16e232da6589c6c5ee01591d50301
jamesparkes <parkesrjames@gmail.com>
2020-06-02 21:09:54 -0500

    use straight SVGs for icons

--- src/components/Button/__stories__/ButtonWithIcon.story.js
+++ src/components/Button/__stories__/ButtonWithIcon.story.js
@@ -1,35 +1,35 @@
 import { action } from '@storybook/addon-actions'
-import CalendarIcon from '../../../icons/CalendarIcon'
 import Button from '../index'
+import Icon from '../../Icon'
 
 export default () => ({
-  components: { Button, CalendarIcon },
+  components: { Button, Icon },
   template: `
     <div class="p-6">
       <div class="flex pb-6">
-        <Button class="mr-6" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
-        <Button class="mr-6" appearance="secondary" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
-        <Button class="mr-6" appearance="text" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
+        <Button class="mr-6" @click.native="action"><Icon name="bell" /></Button>
+        <Button class="mr-6" appearance="secondary" @click.native="action"><Icon name="bell" /></Button>
+        <Button class="mr-6" appearance="text" @click.native="action"><Icon name="bell" /></Button>
       </div>
       <div class="flex pb-6">
-        <Button class="mr-6" appearance="primary" intent="error" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
-        <Button class="mr-6" appearance="secondary" intent="error" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
-        <Button class="mr-6" appearance="text" intent="error" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
+        <Button class="mr-6" appearance="primary" intent="error" @click.native="action"><Icon name="bell" /></Button>
+        <Button class="mr-6" appearance="secondary" intent="error" @click.native="action"><Icon name="bell" /></Button>
+        <Button class="mr-6" appearance="text" intent="error" @click.native="action"><Icon name="bell" /></Button>
       </div>
       <div class="flex pb-6">
-        <Button class="mr-6" appearance="primary" intent="info" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
-        <Button class="mr-6" appearance="secondary" intent="info" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
-        <Button class="mr-6" appearance="text" intent="info" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
+        <Button class="mr-6" appearance="primary" intent="info" @click.native="action"><Icon name="bell" /></Button>
+        <Button class="mr-6" appearance="secondary" intent="info" @click.native="action"><Icon name="bell" /></Button>
+        <Button class="mr-6" appearance="text" intent="info" @click.native="action"><Icon name="bell" /></Button>
       </div>
       <div class="flex pb-6">
-        <Button class="mr-6" appearance="primary" intent="success" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
-        <Button class="mr-6" appearance="secondary" intent="success" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
-        <Button class="mr-6" appearance="text" intent="success" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
+        <Button class="mr-6" appearance="primary" intent="success" @click.native="action"><Icon name="bell" /></Button>
+        <Button class="mr-6" appearance="secondary" intent="success" @click.native="action"><Icon name="bell" /></Button>
+        <Button class="mr-6" appearance="text" intent="success" @click.native="action"><Icon name="bell" /></Button>
       </div>
       <div class="flex pb-6">
-        <Button class="mr-6" appearance="primary" intent="warning" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
-        <Button class="mr-6" appearance="secondary" intent="warning" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
-        <Button class="mr-6" appearance="text" intent="warning" @click.native="action"><CalendarIcon class="h-5 w-5" /></Button>
+        <Button class="mr-6" appearance="primary" intent="warning" @click.native="action"><Icon name="bell" /></Button>
+        <Button class="mr-6" appearance="secondary" intent="warning" @click.native="action"><Icon name="bell" /></Button>
+        <Button class="mr-6" appearance="text" intent="warning" @click.native="action"><Icon name="bell" /></Button>
       </div>
     </div>
   `,
--- src/components/Icon/__stories__/IconDefault.story.js
+++ src/components/Icon/__stories__/IconDefault.story.js
@@ -1,50 +1,25 @@
-import {
-  Bell,
-  CalendarIcon,
-  ChevronDown,
-  ChevronLeft,
-  ChevronRight,
-  ChevronUp,
-  Flag,
-  Info,
-  MapMarked,
-  Minus,
-  Plus,
-  Search,
-  Times,
-} from '../../../icons'
+import Icon from '../index'
 
 export default () => ({
   components: {
-    Bell,
-    CalendarIcon,
-    ChevronDown,
-    ChevronLeft,
-    ChevronRight,
-    ChevronUp,
-    Flag,
-    Info,
-    MapMarked,
-    Minus,
-    Plus,
-    Search,
-    Times,
+    Icon,
   },
   template: `
     <div class="flex items-center p-6">
-      <Bell class="h-5 w-5 mr-3" />
-      <CalendarIcon class="h-5 w-5 mr-3" />
-      <ChevronDown class="h-5 w-5 mr-3" />
-      <ChevronLeft class="h-5 w-5 mr-3" />
-      <ChevronRight class="h-5 w-5 mr-3" />
-      <ChevronUp class="h-5 w-5 mr-3" />
-      <Flag class="h-5 w-5 mr-3" />
-      <Info class="h-5 w-5 mr-3" />
-      <MapMarked class="h-5 w-5 mr-3" />
-      <Minus class="h-5 w-5 mr-3" />
-      <Plus class="h-5 w-5 mr-3" />
-      <Search class="h-5 w-5 mr-3" />
-      <Times class="h-5 w-5 mr-3" />
+      <Icon name="apartment" class="mr-4" />
+      <Icon name="badge" class="mr-4" />
+      <Icon name="bell" class="mr-4" />
+      <Icon name="currency" class="mr-4" />
+      <Icon name="employees" class="mr-4" />
+      <Icon name="file-download" class="mr-4" />
+      <Icon name="file-upload" class="mr-4" />
+      <Icon name="flag" class="mr-4" />
+      <Icon name="hotel" class="mr-4" />
+      <Icon name="info" class="mr-4" />
+      <Icon name="map-pin" class="mr-4" />
+      <Icon name="settings" class="mr-4" />
+      <Icon name="user-delete" class="mr-4" />
+      <Icon name="user-star" class="mr-4" />
     </div>
   `,
 })

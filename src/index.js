331b65b64dc33caf681f737438d9e3348ea258fb
jamesparkes <parkesrjames@gmail.com>
2020-06-03 10:08:01 -0500

    fix: forgot to export icon

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as Button } from './components/Button/index.vue'
+export { default as Icon } from './components/Icon/index.vue'
 export { default as Typography } from './components/Typography/index.vue'

3ac209cbeb0f0d962effbb599260113129c97d6c
Nick Johnson <nickjohnson.dev@gmail.com>
2020-06-03 12:56:53 -0500

    Get sizing working correctly + add Size/Fill stories

--- src/components/Icon/__stories__/Icon.stories.js
+++ src/components/Icon/__stories__/Icon.stories.js
@@ -1,5 +1,7 @@
 import Icon from '../index'
 export { default as Default } from './IconDefault.story'
+export { default as Fill } from './IconFill.story'
+export { default as Size } from './IconSize.story'
 
 export default {
   title: 'Icon',

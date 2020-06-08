894fc9adf22ecb2973ea23bb5051bdc9fa79dc3d
Nick Johnson <8239566+nickjohnson-dev@users.noreply.github.com>
2020-06-08 13:25:28 -0500

    feat: Add iconSize prop to Button (#17)

--- src/components/Button/__stories__/Button.stories.js
+++ src/components/Button/__stories__/Button.stories.js
@@ -1,6 +1,7 @@
 import Button from '../index'
 export { default as Default } from './ButtonDefault.story'
 export { default as Disabled } from './ButtonDisabled.story'
+export { default as IconSize } from './ButtonIconSize.story'
 export { default as Small } from './ButtonSmall.story'
 export { default as WithIcon } from './ButtonWithIcon.story'
 export { default as WithIconSmall } from './ButtonWithIconSmall.story'

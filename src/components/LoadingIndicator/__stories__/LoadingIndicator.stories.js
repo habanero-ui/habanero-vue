8471b5d1fe1a63d8feda8d8485e75d6fa3b033c5
jamesparkes <parkesrjames@gmail.com>
2020-06-09 09:52:36 -0500

    add default story for loading indicator

---
+++ src/components/LoadingIndicator/__stories__/LoadingIndicator.stories.js
@@ -0,0 +1,7 @@
+import LoadingIndicator from '../index'
+export { default as Default } from './LoadingIndicatorDefault.story'
+
+export default {
+  title: 'LoadingIndicator',
+  component: LoadingIndicator,
+}
---
+++ src/components/LoadingIndicator/__stories__/LoadingIndicatorDefault.story.js
@@ -0,0 +1,10 @@
+import LoadingIndicator from '../index'
+
+export default () => ({
+  components: { LoadingIndicator },
+  template: `
+    <div class="p-6">
+      <LoadingIndicator />
+    </div>
+  `,
+})

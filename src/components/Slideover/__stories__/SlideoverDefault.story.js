e2e42df8c9e0b20da09751cfbf4fa3c0fa7e444c
jamesparkes <parkesrjames@gmail.com>
2020-06-10 12:03:02 -0500

    fix up animation and handling open in new window

--- src/components/Slideover/__stories__/SlideoverDefault.story.js
+++ src/components/Slideover/__stories__/SlideoverDefault.story.js
@@ -1,8 +1,14 @@
+import { action } from '@storybook/addon-actions'
 import Slideover from '../index'
 
 export default () => ({
   components: { Slideover },
   template: `
-    <Slideover isOpen="true" />
+    <Slideover @openInNewWindow="action" isOpen="true">
+      <div>This is a test, let's see if it works!</div>
+    </Slideover>
   `,
+  methods: {
+    action: action('Open in new window'),
+  },
 })
--- src/components/Slideover/index.vue
+++ src/components/Slideover/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="slideover" :class="{ 'is-active': isOpen }" @click.self="close">
     <div class="slideover-inner">
-      <div class="slideover-header">
+      <header class="slideover-header">
         <Button
           iconName="chevron-left"
           iconSide="left"
@@ -9,12 +9,14 @@
           @click.native="close"
         />
         <Button
+          v-if="hasOpenInNewWindowListener"
           iconName="new-window"
           iconSide="right"
           iconSize="small"
           text="Open in new window"
+          @click.native="openInNewWindow"
         />
-      </div>
+      </header>
       <div class="slideover-container">
         <slot />
       </div>
@@ -35,9 +37,17 @@ export default {
       type: Boolean,
     },
   },
+  computed: {
+    hasOpenInNewWindowListener() {
+      return this.$listeners && this.$listeners.openInNewWindow
+    },
+  },
   methods: {
     close() {
       this.isOpen = false
+    },
+    openInNewWindow(e) {
+      this.$emit('openInNewWindow', e)
     },
   },
 }
@@ -45,13 +55,13 @@ export default {
 
 <style scoped>
 .slideover {
-  @apply fixed inset-0 z-50 min-h-screen w-full bg-black-half overflow-hidden pointer-events-none opacity-0;
+  @apply absolute inset-0 z-50 min-h-screen w-full bg-black-half overflow-hidden pointer-events-none opacity-0;
   top: 0;
   transition: opacity 300ms ease-in-out;
   will-change: transform;
 }
 .slideover-inner {
-  @apply fixed right-0 flex flex-col max-w-xl w-full h-screen;
+  @apply absolute right-0 flex flex-col max-w-xl w-full h-screen;
   transform: translateX(100%);
   transition: transform 300ms ease-in-out;
   will-change: transform;
@@ -59,13 +69,13 @@ export default {
 .slideover.is-active {
   @apply pointer-events-auto opacity-100;
 }
-.slideover .slideover-inner {
+.slideover.is-active .slideover-inner {
   transform: translateX(0);
 }
 .slideover-header {
   @apply flex justify-between bg-black;
 }
 .slideover-container {
-  @apply relative flex-grow py-8 px-8 w-full bg-white h-full overflow-auto;
+  @apply flex flex-col flex-grow bg-white;
 }
 </style>

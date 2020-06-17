9c5f6fd60aa7fa36cbd595a83465c74f8076bd56
Nick Johnson <nickjohnson.dev@gmail.com>
2020-06-17 11:55:01 -0500

    feat: Update TextInput with all basic features and styles.

--- src/components/TextInput/__stories__/TextInputDefault.story.js
+++ src/components/TextInput/__stories__/TextInputDefault.story.js
@@ -1,10 +1,22 @@
 import { action } from '@storybook/addon-actions'
-import { select, text } from '@storybook/addon-knobs'
+import { boolean, select, text } from '@storybook/addon-knobs'
 import TextInput from '../index'
 
 export default () => ({
   components: { TextInput },
   props: {
+    disabled: {
+      default: boolean('disabled', false),
+    },
+    error: {
+      default: text('error', ''),
+    },
+    helperText: {
+      default: text('helperText', ''),
+    },
+    label: {
+      default: text('label', ''),
+    },
     placeholder: {
       default: text('placeholder', 'Email address'),
     },
@@ -17,11 +29,21 @@ export default () => ({
     },
   },
   template: `
-    <div class="p-6">
-      <TextInput :placeholder="placeholder" :type="type" @input="input" />
+    <div class="flex flex-col p-6">
+      <TextInput
+        :disabled="disabled"
+        :error="error"
+        :helperText="helperText"
+        :label="label"
+        :placeholder="placeholder"
+        :type="type"
+        @click="click"
+        @input="input"
+      />
     </div>
   `,
   methods: {
+    click: action('click'),
     input: action('input'),
   },
 })

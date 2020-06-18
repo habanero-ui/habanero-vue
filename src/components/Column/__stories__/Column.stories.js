8b832cb32e71d4a4d217c7bffe618b3184e81c46
Nick Johnson <nickjohnson.dev@gmail.com>
2020-06-18 12:42:15 -0500

    Add Column component

---
+++ src/components/Column/__stories__/Column.stories.js
@@ -0,0 +1,7 @@
+import Column from '../index'
+export { default as Default } from './ColumnDefault.story'
+
+export default {
+  title: 'Column',
+  component: Column,
+}
---
+++ src/components/Column/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="column"><slot /></div>
+</template>
+
+<script>
+import includes from 'lodash/includes'
+import columnWidths from '../../constants/columnWidths'
+
+export default {
+  props: {
+    width: {
+      default: 'fluid',
+      type: String,
+      validator: getIsWidthValid,
+    },
+  },
+}
+
+function getIsWidthValid(value) {
+  const isValid = includes(columnWidths, value)
+
+  if (!isValid) {
+    // eslint-disable-next-line no-console
+    console.error(
+      'Column: The "width" prop must be one of the following:',
+      String(columnWidths),
+    )
+  }
+
+  return isValid
+}
+</script>

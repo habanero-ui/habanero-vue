0bb935ddd19ed4447a77aae9db8b7ec0f769dabc
Andrew Darts <mandrewdarts@gmail.com>
2020-06-18 11:53:45 -0500

    Remove unused code from previous, A-Z, update story

--- src/components/Select/__stories__/SelectDefault.story.js
+++ src/components/Select/__stories__/SelectDefault.story.js
@@ -5,13 +5,6 @@ import Select from '../index'
 export default () => ({
   components: { Select },
   props: {
-    options: {
-      default: [
-        { label: 'Select 1', value: 1 },
-        { label: 'Select 2', value: 2 },
-        { label: 'Select 3', value: 3 },
-      ],
-    },
     error: {
       default: text('error', ''),
     },
@@ -19,21 +12,26 @@ export default () => ({
       default: text('label', 'This is a label'),
     },
     disabled: {
-      default: boolean('disabled', true),
+      default: boolean('disabled', false),
     },
   },
+  data: () => ({
+    selected: 1,
+  }),
   template: `
     <div class="absolute inset-0 p-4">
       <div class="pb-4">
         <Select 
-          :options="options" 
           :error="error" 
           :label="label"
-          @onSelectChange="onSelectChange"/>
-      </div>
+          :disabled="disabled"
+          v-model="selected">
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+          </Select>
 
-      <div>
-        <Select :options="options" :disabled="disabled"/>
+          Current Selected: {{ selected }}
       </div>
     </div>
   `,
--- src/components/Select/index.vue
+++ src/components/Select/index.vue
@@ -1,109 +1,88 @@
 <template>
-  <Stack space="2">
-    <Typography v-if="label" component="label" variant="label-large">
-      {{ label }}
-    </Typography>
+  <div :class="containerClasses">
+    <Stack space="2">
+      <Typography v-if="label" component="label" variant="label-large">
+        {{ label }}
+      </Typography>
 
-    <div class="flex">
-      <select v-model="selected" :class="selectClasses" v-bind="$attrs">
-        <option value="">
-          Choose an option
-        </option>
-
-        <option
-          v-for="option in options"
-          :key="option.label"
-          :value="option.value"
-          >{{ option.label }}</option
+      <div class="flex">
+        <select
+          :class="selectClasses"
+          :value="value"
+          v-bind="$attrs"
+          v-on="listeners"
+          @input="$emit('input', $event.target.value)"
         >
-      </select>
+          <slot />
+        </select>
 
-      <div class="flex items-center h-10 z-10 -ml-8">
-        <Icon name="chevron-down" :color="isDisabled ? 'subtle' : 'none'" />
+        <div class="flex items-center h-10 z-10 -ml-8">
+          <Icon name="chevron-down" />
+        </div>
       </div>
-    </div>
 
-    <Typography v-if="error" color="error" variant="body-small">
-      {{ error }}
-    </Typography>
-  </Stack>
+      <Typography v-if="error" color="error" variant="body-small">
+        {{ error }}
+      </Typography>
+    </Stack>
+  </div>
 </template>
 
 <script>
-import isEmpty from 'lodash/isEmpty'
-import isUndefined from 'lodash/isUndefined'
-import find from 'lodash/find'
-
 import Icon from '../Icon'
-import Typography from '../Typography'
 import Stack from '../Stack'
+import Typography from '../Typography'
 
 export default {
-  name: 'Select',
-  components: { Icon, Typography, Stack },
+  components: { Icon, Stack, Typography },
   inheritAttrs: false,
 
   props: {
-    options: Array,
-    label: {
-      default: '',
-      type: String,
-    },
     error: {
       default: '',
       type: String,
     },
+    label: {
+      default: '',
+      type: String,
+    },
+    name: {
+      default: '',
+      type: String,
+    },
+    value: {
+      default: '',
+    },
   },
-
-  data: () => ({
-    selected: '',
-    name: Date.now().toString(),
-    open: false,
-  }),
 
   computed: {
-    currentSelectedOption() {
-      return find(this.options, (item) => item.value === this.selected)
+    containerClasses() {
+      return ['container', { 'container--disabled': this.isDisabled }]
     },
-
     isDisabled() {
-      return !isUndefined(this.$attrs.disabled) && isEmpty(this.$attrs.disabled)
+      return this.$attrs.disabled
     },
-
+    listeners() {
+      const { input, ...listeners } = this.$listeners
+      return listeners
+    },
     selectClasses() {
       return ['select', { 'select--has-error': this.error }]
-    },
-  },
-
-  watch: {
-    open(value) {
-      this.$refs.details.open = value
-    },
-
-    selected(value) {
-      this.$emit('onSelectChange', this.currentSelectedOption)
-    },
-  },
-
-  methods: {
-    onToggle(e) {
-      this.open = e.target.open
-    },
-
-    onClickOutside() {
-      if (this.open) {
-        this.open = false
-      }
-    },
-
-    close() {
-      this.open = false
     },
   },
 }
 </script>
 
 <style scoped>
+.container--disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+
+.container--disabled * {
+  cursor: not-allowed;
+}
+
 .select {
   @apply relative px-4 h-10 w-full border border-black rounded-md text-black cursor-pointer transition-colors duration-300 ease-in-out flex items-center;
 }
@@ -121,10 +100,6 @@ export default {
 
 .select:focus {
   @apply border-info outline-none;
-}
-
-.select:disabled {
-  @apply border-grey-400 text-grey-400 cursor-not-allowed;
 }
 
 .select > .selected {

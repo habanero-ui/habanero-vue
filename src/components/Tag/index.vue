fad8d044c6a7e9182c4fdf56a49b871111b619c8
Nick Johnson <8239566+nickjohnson-dev@users.noreply.github.com>
2020-06-30 07:18:28 -0500

    feat: Add Tag component (#50)

---
+++ src/components/Tag/index.vue
@@ -0,0 +1,95 @@
+<template>
+  <Box
+    backgroundColor="border"
+    :class="classes"
+    paddingX="xsmall"
+    :paddingY="onDelete ? 'xxsmall' : 2.25"
+  >
+    <Columns alignY="center" space="xxsmall">
+      <Column>
+        <Typography
+          :colorIsBackground="true"
+          color="border"
+          variant="label-small"
+        >
+          {{ truncatedText }}
+        </Typography>
+      </Column>
+      <Column v-if="onDelete" width="content">
+        <div class="tag__delete-button" @click="handleDeleteButtonClick">
+          <Icon name="close" />
+        </div>
+      </Column>
+    </Columns>
+  </Box>
+</template>
+
+<script>
+import truncate from 'lodash/truncate'
+import Box from '../Box/index'
+import Column from '../Column/index'
+import Columns from '../Columns/index'
+import Icon from '../Icon/index'
+import Typography from '../Typography/index'
+
+export default {
+  components: { Box, Column, Columns, Icon, Typography },
+  props: {
+    maxChars: {
+      default: Infinity,
+      type: Number,
+    },
+    onDelete: {
+      default: undefined,
+      type: Function,
+    },
+    text: {
+      default: '',
+      type: String,
+    },
+    value: {
+      default: '',
+      type: [Number, String],
+    },
+  },
+  computed: {
+    classes() {
+      return ['tag']
+    },
+
+    truncatedText() {
+      return truncate(this.text || this.value, {
+        length: this.maxChars,
+        omission: '…',
+      })
+    },
+  },
+  methods: {
+    handleDeleteButtonClick() {
+      if (!this.onDelete) return
+
+      this.onDelete(this.value)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.tag {
+  @apply inline-flex rounded-md;
+}
+.tag__delete-button {
+  @apply relative cursor-pointer -mr-1 rounded-full;
+}
+.tag__delete-button::after {
+  @apply absolute inset-0 pointer-events-none opacity-0 rounded-full transition-opacity duration-100 ease-in-out;
+  background-color: black;
+  content: '';
+}
+.tag__delete-button:hover::after {
+  opacity: 0.1;
+}
+.tag__delete-button:active::after {
+  opacity: 0.25;
+}
+</style>

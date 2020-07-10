4d125af973e0560226a75e77fc28ea6e99702e98
Nick Johnson <nickjohnson.dev@gmail.com>
2020-07-10 12:50:16 -0500

    Add more interesting layout to DataTableMultilineCell for story

--- src/components/DataTable/__stories__/DataTableMultilineCell.vue
+++ src/components/DataTable/__stories__/DataTableMultilineCell.vue
@@ -1,12 +1,18 @@
 <template>
   <Box padding="medium">
-    <Stack space="xxsmall">
-      <Typography
-        v-for="rating in ratings"
-        :key="rating.source"
-        variant="body-extra-small"
-        >{{ rating.source }}: {{ rating.value }}</Typography
-      >
+    <Stack space="xsmall">
+      <Columns v-for="rating in ratings" :key="rating.source" space="xxsmall">
+        <Column>
+          <Typography color="subtle" variant="label-small">
+            {{ rating.source }}:
+          </Typography>
+        </Column>
+        <Column width="content">
+          <Typography variant="body-extra-small">
+            {{ rating.value }}
+          </Typography>
+        </Column>
+      </Columns>
     </Stack>
   </Box>
 </template>
@@ -15,11 +21,13 @@
 import random from 'lodash/random'
 import getWithAccessor from '../../../helpers/getWithAccessor'
 import Box from '../../Box/index'
+import Column from '../../Column/index'
+import Columns from '../../Columns/index'
 import Stack from '../../Stack/index'
 import Typography from '../../Typography/index'
 
 export default {
-  components: { Box, Stack, Typography },
+  components: { Box, Column, Columns, Stack, Typography },
   props: {
     column: {
       default: () => ({}),
@@ -35,6 +43,8 @@ export default {
       return [
         { source: 'Critics', value: this.row.rating.toFixed(1) },
         { source: 'Users', value: random(0.0, 10.0, true).toFixed(1) },
+        { source: 'Random', value: random(0.0, 10.0, true).toFixed(1) },
+        { source: 'Metacritic', value: random(0.0, 10.0, true).toFixed(1) },
       ]
     },
     criticRating() {

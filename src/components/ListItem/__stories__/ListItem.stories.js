248e371833a2b4014b4a22031ef3f8d9bd78df45
jamesparkes <parkesrjames@gmail.com>
2020-07-13 14:58:04 -0500

    reorder list item stories

--- src/components/ListItem/__stories__/ListItem.stories.js
+++ src/components/ListItem/__stories__/ListItem.stories.js
@@ -1,9 +1,9 @@
 import ListItem from '../index'
+export { default as SingleLine } from './ListItemSingleLine.story'
 export { default as DoubleLine } from './ListItemDoubleLine.story'
+export { default as TripleLine } from './ListItemTripleLine.story'
 export { default as Navigation } from './ListItemNavigation.story'
 export { default as NotActionable } from './ListItemNotActionable.story'
-export { default as SingleLine } from './ListItemSingleLine.story'
-export { default as TripleLine } from './ListItemTripleLine.story'
 export { default as WithIcon } from './ListItemWithIcon.story'
 export { default as WithStatus } from './ListItemWithStatus.story'
 

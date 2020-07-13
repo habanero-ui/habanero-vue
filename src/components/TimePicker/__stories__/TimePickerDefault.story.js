8603fc76bbff954857ce219334b5e54d652beeb8
jamesparkes <parkesrjames@gmail.com>
2020-07-13 13:06:44 -0500

    first pass at time picker component

--- src/components/TimePicker/__stories__/TimePickerDefault.story.js
+++ src/components/TimePicker/__stories__/TimePickerDefault.story.js
@@ -1,10 +1,54 @@
+import { action } from '@storybook/addon-actions'
+import { boolean, select, text } from '@storybook/addon-knobs'
+
+import icons from '../../../constants/icons'
 import TimePicker from '../index'
 
 export default () => ({
   components: { TimePicker },
+  props: {
+    disabled: {
+      default: boolean('disabled', false),
+    },
+    error: {
+      default: text('error', ''),
+    },
+    helperText: {
+      default: text('helperText', 'Enter the time your shift started'),
+    },
+    iconName: {
+      default: select('iconName', [undefined, ...icons]),
+    },
+    label: {
+      default: text('label', 'Start Time'),
+    },
+    placeholder: {
+      default: text('placeholder', 'Select time...'),
+    },
+    value: {
+      default: text(
+        'value',
+        new Date().toLocaleTimeString('en-US', {
+          hour: '2-digit',
+          minute: '2-digit',
+        }),
+      ),
+    },
+  },
   template: `
     <div class="flex flex-col p-6">
-      <TimePicker />
+      <TimePicker
+        :disabled="disabled"
+        :error="error"
+        :helperText="helperText"
+        :label="label"
+        :iconName="iconName"
+        :onValueChange="onValueChange"
+        :placeholder="placeholder"
+        :value="value" />
     </div>
   `,
+  methods: {
+    onValueChange: action('onValueChange'),
+  },
 })
--- src/components/TimePicker/index.vue
+++ src/components/TimePicker/index.vue
@@ -1,7 +1,76 @@
 <template>
-  <div>Time Picker</div>
+  <TextInput
+    :ref="'timePicker'"
+    :disabled="disabled"
+    :error="error"
+    :helperText="helperText"
+    iconName="time-clock"
+    iconSide="right"
+    :label="label"
+    :placeholder="placeholder"
+    :value="value"
+  />
 </template>
 
 <script>
-export default {}
+import flatpickr from 'flatpickr'
+
+import TextInput from '../TextInput/index'
+
+export default {
+  components: {
+    TextInput,
+  },
+  props: {
+    disabled: {
+      default: false,
+      type: Boolean,
+    },
+    error: {
+      default: '',
+      type: String,
+    },
+    helperText: {
+      default: '',
+      type: String,
+    },
+    label: {
+      default: '',
+      type: String,
+    },
+    onValueChange: {
+      default: undefined,
+      type: Function,
+    },
+    placeholder: {
+      default: '',
+      type: String,
+    },
+    value: {
+      default: '',
+      type: String,
+    },
+  },
+  data: () => ({
+    picker: null,
+  }),
+  beforeDestroy() {
+    if (this.picker) {
+      this.picker.destroy()
+    }
+  },
+  mounted() {
+    this.picker = flatpickr(this.$refs.timePicker.$refs.input, {
+      dateFormat: 'h:i K',
+      enableTime: true,
+      onChange: this.handleInputChange,
+      noCalendar: true,
+    })
+  },
+  methods: {
+    handleInputChange() {
+      this.onValueChange(this.$refs.timePicker.$refs.input.value)
+    },
+  },
+}
 </script>

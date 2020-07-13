a3c469b539bf971292c94a4cc7c84002aadc2c25
jamesparkes <parkesrjames@gmail.com>
2020-07-13 16:23:04 -0500

    add date range story, why not

--- src/components/DatePicker/__stories__/DatePicker.stories.js
+++ src/components/DatePicker/__stories__/DatePicker.stories.js
@@ -2,6 +2,7 @@ import DatePicker from '../index'
 export { default as Default } from './DatePickerDefault.story'
 export { default as SingleDate } from './DatePickerSingleDate.story'
 export { default as MultiDate } from './DatePickerMultiDate.story'
+export { default as RangeDate } from './DatePickerRangeDate.story'
 
 export default {
   title: 'DatePicker',

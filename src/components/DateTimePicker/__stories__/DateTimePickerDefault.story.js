deead62bc6457e7de5b6d9e16ab79ab2306cc3e8
jamesparkes <parkesrjames@gmail.com>
2020-07-13 10:40:35 -0500

    feat(DateTimePicker): Add DateTimePicker component

---
+++ src/components/DateTimePicker/__stories__/DateTimePickerDefault.story.js
@@ -0,0 +1,10 @@
+import DateTimePicker from '../index'
+
+export default () => ({
+  components: { DateTimePicker },
+  template: `
+    <div class="flex flex-col p-6">
+      <DateTimePicker />
+    </div>
+  `,
+})
---
+++ src/components/DateTimePicker/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Date Time Picker</div>
+</template>
+
+<script>
+export default {}
+</script>

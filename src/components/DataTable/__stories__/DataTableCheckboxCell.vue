80f275df8293d063bb3b698d160eb752507299b9
Nick Johnson <nickjohnson.dev@gmail.com>
2020-07-17 08:29:25 -0500

    chore(stories): Add example of cell with custom event to DataTableCustomCells.

---
+++ src/components/DataTable/__stories__/DataTableCheckboxCell.vue
@@ -0,0 +1,39 @@
+<template>
+  <Box padding="medium">
+    <Checkbox
+      :isChecked="seen"
+      :onIsCheckedChange="handleCheckboxIsCheckedChange"
+    />
+  </Box>
+</template>
+
+<script>
+import Box from '../../Box/index'
+import Checkbox from '../../Checkbox/index'
+
+export default {
+  components: { Box, Checkbox },
+  props: {
+    column: {
+      default: () => ({}),
+      type: Object,
+    },
+    row: {
+      default: () => ({}),
+      type: Object,
+    },
+  },
+  computed: {
+    seen() {
+      return this.row.seen
+    },
+  },
+  methods: {
+    handleCheckboxIsCheckedChange(isChecked) {
+      if (!this.column.onRowChange) return
+
+      this.column.onRowChange({ ...this.row, seen: isChecked })
+    },
+  },
+}
+</script>
--- src/components/DataTable/__stories__/DataTableCustomCells.story.js
+++ src/components/DataTable/__stories__/DataTableCustomCells.story.js
@@ -1,4 +1,7 @@
+import map from 'lodash/map'
+
 import DataTable from '../index'
+import DataTableCheckboxCell from './DataTableCheckboxCell'
 import DataTableMultilineCell from './DataTableMultilineCell'
 import DataTableProfileCell from './DataTableProfileCell'
 import tableData from './tableData.js'
@@ -6,45 +9,55 @@ import tableData from './tableData.js'
 export default () => ({
   components: { DataTable },
   data: () => ({
-    columns: [
-      {
-        name: 'Rank',
-        accessor: 'id',
-      },
-      {
-        name: 'Movie',
-        accessor: 'movie.name',
-        cellComponent: DataTableProfileCell,
-      },
-      {
-        name: 'Director',
-        accessor: ({ movie }) => {
-          return `${movie.director.firstName} ${movie.director.lastName}`
-        },
-      },
-      {
-        name: 'Budget',
-        accessor: ({ movie }) => {
-          return movie.costCents
-            ? `$${(movie.costCents / 100)
-                .toFixed(2)
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
-            : 'N/A'
-        },
-      },
-      {
-        name: 'Release Year',
-        accessor: 'movie.year',
-      },
-      {
-        name: 'IMDB Rating',
-        accessor: 'rating',
-        cellComponent: DataTableMultilineCell,
-      },
-    ],
     rows: tableData,
   }),
+  computed: {
+    columns() {
+      return [
+        {
+          name: 'Rank',
+          accessor: 'id',
+        },
+        {
+          name: 'Seen?',
+          accessor: 'seen',
+          cellComponent: DataTableCheckboxCell,
+          onRowChange: this.handleRowChange,
+        },
+        {
+          name: 'Movie',
+          accessor: 'movie.name',
+          cellComponent: DataTableProfileCell,
+        },
+        {
+          name: 'Director',
+          accessor: ({ movie }) => {
+            return `${movie.director.firstName} ${movie.director.lastName}`
+          },
+        },
+        {
+          name: 'Budget',
+          accessor: ({ movie }) => {
+            return movie.costCents
+              ? `$${(movie.costCents / 100)
+                  .toFixed(2)
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+              : 'N/A'
+          },
+        },
+        {
+          name: 'Release Year',
+          accessor: 'movie.year',
+        },
+        {
+          name: 'IMDB Rating',
+          accessor: 'rating',
+          cellComponent: DataTableMultilineCell,
+        },
+      ]
+    },
+  },
   template: `
     <div class="p-6"> 
       <DataTable
@@ -53,4 +66,11 @@ export default () => ({
       />
     </div>
   `,
+  methods: {
+    handleRowChange(changedRow) {
+      this.rows = map(this.rows, (row) =>
+        row.id === changedRow.id ? changedRow : row,
+      )
+    },
+  },
 })

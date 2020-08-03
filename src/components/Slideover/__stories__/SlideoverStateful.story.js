58279a9180f126c88c6e47c645c7ad978441dcd7
Nick Johnson <nickjohnson.dev@gmail.com>
2020-08-03 16:17:09 -0500

    docs(Stories): Add SlideoverStateful story

---
+++ src/components/Slideover/__stories__/SlideoverStateful.story.js
@@ -0,0 +1,33 @@
+import Button from '../../Button/index'
+import Slideover from '../index'
+
+export default () => ({
+  components: { Button, Slideover },
+  data: () => ({
+    isOpen: false,
+  }),
+  template: `
+    <div class="absolute flex flex-col items-start inset-0 p-6">
+      <div>
+        <Button
+          text="Open Slideover"
+          @click.native="handleOpenSlideoverClick"
+        />
+      </div>
+      <Slideover
+        :isOpen="isOpen"
+        :onIsOpenChange="handleIsOpenChange">
+        <div>Some Content</div>
+      </Slideover>
+    </div>
+  `,
+  methods: {
+    handleIsOpenChange(isOpen) {
+      this.isOpen = isOpen
+    },
+
+    handleOpenSlideoverClick() {
+      this.isOpen = true
+    },
+  },
+})

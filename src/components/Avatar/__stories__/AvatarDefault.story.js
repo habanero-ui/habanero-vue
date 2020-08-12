7f7764d0f44195e55cbe5deabcc8014ba21277d3
jamesparkes <parkesrjames@gmail.com>
2020-08-12 09:32:23 -0500

    upgrade storybook

--- src/components/Avatar/__stories__/AvatarDefault.story.js
+++ src/components/Avatar/__stories__/AvatarDefault.story.js
@@ -1,26 +1,20 @@
-import { text } from '@storybook/addon-knobs'
-
 import Avatar from '../index'
 
-export default () => ({
-  components: { Avatar },
-  props: {
-    firstName: {
-      default: text('firstName', 'Patches'),
-    },
-    image: {
-      default: text('image', 'http://placekitten.com/256/256'),
-    },
-    lastName: {
-      default: text('lastName', "O'Houlihan"),
-    },
-  },
-  template: `
-    <div class="flex flex-start p-6"> 
-      <Avatar
-        :firstName="firstName"
-        :image="image"
-        :lastName="lastName" />
-    </div>
-  `,
-})
+// export default () => ({
+//   components: { Avatar },
+//   template: `
+//     <div class="flex flex-start p-6">
+//       <Avatar
+//         :firstName="firstName"
+//         :image="image"
+//         :lastName="lastName" />
+//     </div>
+//   `,
+// })
+
+export const Default = (args) => <Avatar {...args} />
+Default.args = {
+  firstName: 'Patches',
+  image: 'http://placekitten.com/256/256',
+  lastName: `O'Houlihan`,
+}

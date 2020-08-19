6739fbfd0a8f0872e8a165f0ec6cfad2ea4bb1b8
jamesparkes <parkesrjames@gmail.com>
2020-08-19 10:14:09 -0500

    remove Color component, no longer needed

--- src/components/Color/__stories__/Color.stories.js
+++
@@ -1,8 +0,0 @@
-import Color from '../index'
-export { default as Default } from './ColorDefault.story'
-
-export default {
-  title: 'Components/Color',
-  component: Color,
-  parameters: { layout: 'padded' },
-}

3c9c71c3facb6c2ddbf579313b4d9164fc4eb588
Nick Johnson <nickjohnson.dev@gmail.com>
2020-10-19 13:19:30 -0500

    Update Vue to latest 2.x version

--- src/components/__internal/ClickOutsideDetector/index.vue
+++ src/components/__internal/ClickOutsideDetector/index.vue
@@ -6,18 +6,20 @@ export default {
       type: Function,
     },
   },
+  data: () => ({
+    listener: undefined,
+  }),
   mounted() {
-    const listener = (e) => {
+    this.listener = (e) => {
       if (e.target === this.$el || this.$el.contains(e.target)) return
 
       this.onClickOutside(e)
     }
 
-    document.addEventListener('click', listener)
-
-    this.$once('hook:beforeDestroy', () => {
-      document.removeEventListener('click', listener)
-    })
+    document.addEventListener('click', this.listener)
+  },
+  beforeDestroy() {
+    document.removeEventListener('click', this.listener)
   },
   render() {
     return this.$slots.default[0]
